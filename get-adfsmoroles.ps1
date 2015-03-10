84182c9c0dd14183956f45dba11ff6763f3b27a6
strange-walker <strange.walker.ua@gmail.com>
2015-03-10 14:33:16 +0200

    get-fsmo

--- get-adfsmoroles.ps1
+++ get-adfsmoroles.ps1
@@ -1,1 +1,11 @@
-
+﻿function Get-FSMORoles {
+    $domain = Get-ADDomain
+    $forest = Get-ADForest
+    $fsmo = @{InfrastructureMaster = $domain.InfrastructureMaster 
+              PDCEmulator = $domain.PDCEmulator 
+              RIDMaster = $domain.RIDMaster 
+              DomainNamingMaster = $forest.DomainNamingMaster 
+              SchemaMaster = $forest.SchemaMaster
+              }
+    $fsmo
+}
