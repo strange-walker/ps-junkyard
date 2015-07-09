e80257a05476c5150bb29e1524cca7ee7bf3e6bc
strange-walker <strange.walker.ua@gmail.com>
2015-07-09 20:10:25 +0300

    cleanup

--- close-window.ps1
+++ close-window.ps1
@@ -1,9 +1,8 @@
 ﻿add-type -AssemblyName microsoft.VisualBasic
 add-type -AssemblyName System.Windows.Forms
 
-#for test. remove this
+#for test. remove this 2 lines
 calc
-
 start-sleep -Milliseconds 500
 
 #replace "Калькулятор" with actual WINDOW TITLE
