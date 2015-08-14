57f1ca12978b79eef5343e1dccea85c02cfddedc
strange-walker <strange.walker.ua@gmail.com>
2015-08-14 14:20:46 +0300

    useful stuff

---
+++ AD queries.ps1
@@ -0,0 +1,3 @@
+﻿
+# Список учеток неактивных более 90 дней
+Search-ADAccount -AccountInactive -TimeSpan 90.00:00:00 | ?{$_.enabled -eq $true} | Select-Object name, objectclass, enabled, lastlogondate | Export-Csv D:\inactive.csv  -UseCulture
