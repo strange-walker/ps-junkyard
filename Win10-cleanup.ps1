b08c3a17aadf23085eb4cb03b19253d0732b949d
strange-walker <strange.walker.ua@gmail.com>
2015-10-19 10:50:19 +0300

    destrooyyy

---
+++ Win10-cleanup.ps1
@@ -0,0 +1,16 @@
+﻿#remove junk apps
+
+Get-AppxPackage -Name Microsoft.3DBuilder | Remove-AppxPackage
+Get-AppxPackage -Name Microsoft.BingSports | Remove-AppxPackage
+Get-AppxPackage -Name Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
+Get-AppxPackage -Name Microsoft.People | Remove-AppxPackage
+Get-AppxPackage -Name Microsoft.BingWeather | Remove-AppxPackage
+Get-AppxPackage -Name Microsoft.BingFinance | Remove-AppxPackage
+Get-AppxPackage -Name Microsoft.SkypeApp | Remove-AppxPackage
+
+
+
+#onedrive extermination
+
+Stop-Process -Name OneDrive
+C:\Windows\SysWOW64\OneDriveSetup.exe /uninstall
