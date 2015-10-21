10e1574e5f436233816cfac511a5a92fc40e32ca
strange-walker <strange.walker.ua@gmail.com>
2015-10-21 18:38:47 +0300

    1

--- Win10-cleanup.ps1
+++ Win10-cleanup.ps1
@@ -1,38 +1,5 @@
 ﻿#remove junk apps
-
-Microsoft.AAD.BrokerPlugin
-Microsoft.Windows.CloudExperienceHost
-Microsoft.Windows.ShellExperienceHost
-windows.immersivecontrolpanel
-Microsoft.Windows.Cortana
-Microsoft.AccountsControl
-Microsoft.BioEnrollment
-Microsoft.LockApp
-Microsoft.MicrosoftEdge
-Microsoft.Windows.AssignedAccessLockApp
-Microsoft.Windows.ContentDeliveryManager
-Microsoft.Windows.ParentalControls
-Microsoft.WindowsFeedback
-Windows.ContactSupport
-Windows.MiracastView
-Windows.PrintDialog
-Windows.PurchaseDialog
-microsoft.windowscommunicationsapps
-Microsoft.Windows.Photos
-
-Microsoft.WindowsStore
-windows.devicesflow
-Microsoft.WindowsAlarms
-Microsoft.WindowsSoundRecorder
-Microsoft.WindowsPhone
-Microsoft.WindowsMaps
-Microsoft.WindowsDVDPlayer
-Microsoft.Office.OneNote
-Microsoft.MicrosoftSolitaireCollection
-Microsoft.MicrosoftOfficeHub
-Microsoft.Appconnector
-
-$applist = @(
+$JunkAppsList = @(
                 "Microsoft.3DBuilder"
                 "Microsoft.BingSports"
                 "Microsoft.BingNews"
@@ -48,20 +15,47 @@ $applist = @(
                 "Microsoft.XboxApp"
                 "Microsoft.XboxGameCallableUI"
                 "Microsoft.XboxIdentityProvider"
+                "microsoft.windowscommunicationsapps"
+                "Windows.MiracastView"
+                "Windows.PurchaseDialog"
                 
             )
-
-foreach ($app in $applist) {
+$OtherApps = @(
+                "Microsoft.AAD.BrokerPlugin"
+                "Microsoft.Windows.CloudExperienceHost"
+                "Microsoft.Windows.ShellExperienceHost"
+                "windows.immersivecontrolpanel"
+                "Microsoft.Windows.Cortana"
+                "Microsoft.AccountsControl"
+                "Microsoft.BioEnrollment"
+                "Microsoft.LockApp"
+                "Microsoft.MicrosoftEdge"
+                "Microsoft.Windows.AssignedAccessLockApp"
+                "Microsoft.Windows.ContentDeliveryManager"
+                "Microsoft.Windows.ParentalControls"
+                "Microsoft.WindowsFeedback"
+                "Windows.ContactSupport"
+                "Windows.PrintDialog"
+                "Microsoft.Windows.Photos"
+                "Microsoft.WindowsStore"
+                "windows.devicesflow"
+                "Microsoft.WindowsAlarms"
+                "Microsoft.WindowsSoundRecorder"
+                "Microsoft.WindowsPhone"
+                "Microsoft.WindowsMaps"
+                "Microsoft.WindowsDVDPlayer"
+                "Microsoft.Office.OneNote"
+                "Microsoft.MicrosoftOfficeHub"
+                "Microsoft.Appconnector"
+            )
+foreach ($app in $JunkAppsList) {
     Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage
     }
 
 
 #onedrive extermination
-
 Stop-Process -Name OneDrive
 C:\Windows\SysWOW64\OneDriveSetup.exe /uninstall
-
-
 New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
 Remove-Item -Path "hkcr:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse
 Remove-Item -Path "hkcr:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse
