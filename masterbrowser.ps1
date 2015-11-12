4754befb2762475c85518f2e0649cb502a2e5e46
strange-walker <strange.walker.ua@gmail.com>
2015-11-12 18:40:45 +0200

    1

---
+++ masterbrowser.ps1
@@ -0,0 +1,30 @@
+﻿$test = net view
+
+$netbios = @()
+foreach ($item in $test)
+{
+#$item[1]
+     if ($item[1] -eq "\" ) 
+        { 
+        $temp = $item.Trim("\"," ")
+        #$temp = $temp.Substring(0, $temp.IndexOf(' '))
+        $temp = $temp.split(' ')[0] 
+        $netbios += $temp
+        }
+
+}
+
+$netbios
+
+
+
+$out = @()
+
+foreach ($item in $netbios)
+{
+    $out += (nbtstat -a $item)
+}
+
+
+$out > D:\scripts\mb.txt
+
