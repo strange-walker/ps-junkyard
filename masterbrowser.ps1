a0bb8c2bdcadd784bb297290f1546e2b0b92db9c
strange-walker <strange.walker.ua@gmail.com>
2015-11-16 18:43:12 +0200

    oops

--- masterbrowser.ps1
+++ masterbrowser.ps1
@@ -2,7 +2,7 @@
 $netbios = @()
 
 #extract netbios names from raw data
-foreach ($item in $NewRat)
+foreach ($item in $NetRaw)
 {
      if ($item[1] -eq "\" ) 
         { 
@@ -14,6 +14,7 @@ foreach ($item in $NewRat)
 }
 
 
+#query each machine and test if it is master browser
 foreach ($item in $netbios)
 {
 
