b34091b4d44e19e34d88a282dbf91ef627886ba4
strange-walker <strange.walker.ua@gmail.com>
2015-12-01 23:50:42 +0200

    win32

--- validate-path.ps1
+++ validate-path.ps1
@@ -28,37 +28,31 @@ function Validate-Path
         $drive = $false
         $folder = $false
 
-        $driveletter = $path.Substring(0,2)
-        
         $folders = $Path.Substring(3)
         }
     Process
         {
         #verify disk type (hdd, removable or network) and connectivity
-        $driveobject = Get-WmiObject -Class Win32_LogicalDisk | Where-Object {$_.DeviceID -eq $drive -and $_.DriveType -match '[234]'}
+        $driveletter = $path.Substring(0,2)
+        #drive types: flash = 2, hdd = 3, network disk = 4
+        $driveobject = Get-WmiObject -Class Win32_LogicalDisk | Where-Object {$_.DeviceID -eq $driveletter -and $_.DriveType -match '[234]'}
         if ($driveobject.freespace -ne $null)
-        {
-            
-        }
+            {
+            $drive = $true
+            }
         
+        # list of forbidden symbols https://msdn.microsoft.com/en-us/library/windows/desktop/aa365247%28v=vs.85%29.aspx#win32_file_namespaces
+        $bannedobjects = ("CON","PRN","AUX","NUL","COM1","COM2","COM3","COM4","COM5","COM6","COM7","COM8","COM9","LPT1","LPT2","LPT3","LPT4","LPT5","LPT6","LPT7","LPT8","LPT9")
+        $bannedsymbols = ("<",">",":","/","\\","|","?","*",'"')
+
         }
     End
         {
+        $drive
+        $folder
+        $verdict = $drive -and $folder
         $verdict
         }
 }
 
-
-Get-WmiObject -Class Win32_LogicalDisk
-
-$path = 'D:\test\150806'
-$drive = $path.Substring(0,2)
-$folders = $Path.Substring(3)
-$drive
-$folders
-
-flash = 2
-hdd = 3
-network = 4
-
-Get-WmiObject -Class Win32_LogicalDisk | Where-Object {$_.DeviceID -eq $drive -and $_.DriveType -match '[234]'}
+NUL.txt is not recommended. For more information, see Namespaces.
