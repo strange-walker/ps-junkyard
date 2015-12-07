ecea6e0db7b82e98ebfbbae3dac80de63fa186dc
strange-walker <strange.walker.ua@gmail.com>
2015-12-08 00:00:27 +0200

    0.0

---
+++ get-hardwareinfo-parallel.ps1
@@ -0,0 +1,40 @@
+﻿<#
+.Synopsis
+   Краткое описание
+.DESCRIPTION
+   Длинное описание
+.EXAMPLE
+   Пример использования этого командлета
+.EXAMPLE
+   Еще один пример использования этого командлета
+#>
+function Get-HardwareInfo
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+        # Справочное описание параметра 1
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Scope='localhost'
+    )
+
+    Begin
+    {
+
+    }
+    Process {
+        if ($Scope.count -gt 1) {
+            $result = $Scope | Get-HardwareInfo
+            }
+        else {
+            #actual queries here
+            }
+
+        }
+    End
+    {
+    }
+}
