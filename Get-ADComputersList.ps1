e0b91bcdf237a968526e95c4cfa55d612a2e8242
strange-walker <strange.walker.ua@gmail.com>
2015-12-30 18:43:18 +0200

    1.2
    1.2

--- Get-ADComputersList.ps1
+++ Get-ADComputersList.ps1
@@ -1,6 +1,6 @@
 ﻿<#
 .Synopsis
-   Short description
+   Custom-filtered Get-ADComputer
 .DESCRIPTION
    Long description
 .EXAMPLE
@@ -8,53 +8,32 @@
 .EXAMPLE
    Another example of how to use this cmdlet
 #>
-function Get-ADComputersList
-{
+function Get-ADComputersList {
     [CmdletBinding()]
-    [Alias()]
-    [OutputType([int])]
-    Param
-    (
+    Param (
         # Param1 help description
         [Parameter(Mandatory=$true)]
-        [validateset("All", "Servers", "Workstations")]$Scope,
+        [validateset("All", "Servers", "Workstations")]$Scope ,
 
         # Specifies
-        [int][ValidateRange(1,60)] $Active = $null
-    )
-
-    Begin
-    {
-    $SpecialProperties = @('LastLogonDate', 'OperatingSystem', 'Name')
-    switch ($Scope)
-        {
-        "All"              {$filter = "*"}
-        "Workstations"     {$filter = "OperatingSystem -like '* XP *' -or 
-                                       OperatingSystem -like '* 7 *' -or 
-                                       OperatingSystem -like '* 8.1 *' -or
-                                       OperatingSystem -like '* 10 *'"}
-        "Servers"          {$filter = "OperatingSystem -like '*Server*'"}
-        Default            {Throw "If you're seeing this, something has gone terribly wrong. Run, you fool." }
+        [int][ValidateRange(1,90)] $Active = $null
+        )
+    Process {
+        $SpecialProperties = @('LastLogonDate', 'OperatingSystem', 'Name')
+        switch ($Scope) {
+            "All"              {$filter = "*"}
+            "Workstations"     {$filter = "OperatingSystem -like '* XP *' -or 
+                                           OperatingSystem -like '* 7 *' -or 
+                                           OperatingSystem -like '* 8.1 *' -or
+                                           OperatingSystem -like '* 10 *'"}
+            "Servers"          {$filter = "OperatingSystem -like '*Server*'"}
+            Default            {Throw "If you're seeing this, something has gone terribly wrong. Run, you fool." }
+            }
+        $result = Get-ADComputer  -Properties $SpecialProperties -Filter $filter
+        if ($Active -ne 0) {
+            $result = $result | Where-Object -Property LastLogonDate -gt (get-date).adddays(-$Active)
+            }
+        $result
         }
-    $result = Get-ADComputer  -Properties $SpecialProperties -Filter $filter
 
-    if ($Active -ne 0)
-    {
-        $result = $result | Where-Object -Property LastLogonDate -gt (get-date).adddays(-$Active)
     }
-    }
-    Process
-    {
-   
-    }
-    End
-    {
-    $result
-    }
-}
-
-
-#single pc object
-
-$full = Get-ADComputer  -Properties * -Filter {Name -eq 'algernon'}
-#$full
