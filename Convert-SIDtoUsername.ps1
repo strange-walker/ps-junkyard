147537407c5dddb5233990c28cd779d5159d3c3a
strange-walker <strange.walker.ua@gmail.com>
2016-08-03 07:37:40 +0300

    sid

---
+++ Convert-SIDtoUsername.ps1
@@ -0,0 +1,29 @@
+﻿ <#
+ .Synopsis
+    Краткое описание
+ .DESCRIPTION
+    Длинное описание
+ .EXAMPLE
+    Пример использования этого командлета
+ .EXAMPLE
+    Еще один пример использования этого командлета
+ #>
+ function Convert-SIDtoUsername {
+     [CmdletBinding()]
+     [Alias()]
+     [OutputType([int])]
+     Param (
+         # Справочное описание параметра 1
+         [Parameter(Mandatory=$true,
+                    ValueFromPipelineByPropertyName=$true,
+                    Position=0)]
+         $SID
+         )
+     Begin {     }
+     Process {
+        $objSID = New-Object System.Security.Principal.SecurityIdentifier($SID)
+        $objUser = $objSID.Translate( [System.Security.Principal.NTAccount] )
+        $objUser.Value 
+        }
+     End {     }
+ }
